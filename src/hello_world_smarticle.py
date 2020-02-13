d41a3a3b105cf4bd41ae81f1df996e1598623e9f
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-13 15:54:01 -0600

    finished smarticle object and updated hello world file

--- src/hello_world_smarticle.py
+++ src/hello_world_smarticle.py
@@ -1,7 +1,7 @@
 import pybullet as p
 import time
 import pybullet_data
-from pdb import set_trace as bp
+from smarticle_simulation_object import SmarticleSimulationObject as sso
 
 import numpy as np
 
@@ -20,66 +20,37 @@ p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+maxvel = 6.
+dt = time_to_steps(0.45)
 x = 0.02
 r = p.loadURDF(ring_path, basePosition = [0,0,0])
-s1 = p.loadURDF(urdf_path, basePosition = [-0.027,x+0.03,0])
-s2 = p.loadURDF(urdf_path, basePosition = [-0.027,x+0,0])
-s3 = p.loadURDF(urdf_path, basePosition = [-0.027,x-0.03,0])
-s4 = p.loadURDF(urdf_path, basePosition = [-0.027,x-0.06,0])
-s5 = p.loadURDF(urdf_path, basePosition = [-0.027,x-0.09,0])
+s1 = sso(p, urdf_path, maxvel, basePosition = [-0.027,x+0.03,0])
+s2 = sso(p, urdf_path, maxvel, basePosition = [-0.027,x+0,0])
+s3 = sso(p, urdf_path, maxvel, basePosition = [-0.027,x-0.03,0])
+s4 = sso(p, urdf_path, maxvel, basePosition = [-0.027,x-0.06,0])
+s5 = sso(p, urdf_path, maxvel, basePosition = [-0.027,x-0.09,0])
+smarticles = [s1, s2, s3, s4, s5]
+R = [-1.7,1.7,1.7,-1.7]
+L = [1.7,1.7,-1.7,-1.7]
+for s in smarticles:
+    s.load_gait(np.array([L,R]),dt)
 
-R = [-1.57,1.57,1.57,-1.57]
-L = [1.57,1.57,-1.57,-1.57]
-j1 = np.random.randint(0,4)
-dt = time_to_steps(0.45)
-n1 = int(dt*np.random.rand())
-n2 = int(dt*np.random.rand())
-n3 = int(dt*np.random.rand())
-n4 = int(dt*np.random.rand())
-n5 = int(dt*np.random.rand())
 
-j1 = np.random.randint(0,4)
-j2 = np.random.randint(0,4)
-j3 = np.random.randint(0,4)
-j4 = np.random.randint(0,4)
-j5 = np.random.randint(0,4)
-
-t0 = time.time()
-tlast = t0
-control =  p.POSITION_CONTROL
-n=4
-maxvel = 5.95*np.ones(10)+0.2*np.random.rand(10)
-joints = [0,1]
-time.sleep(3)
 t_steps = time_to_steps(1200)
 for i in range (t_steps):
     p.stepSimulation()
-    time.sleep(1./4800.)
-    if i%dt==n1:
-        j1 = j1%len(R)
-        p.setJointMotorControl2(s1,0, control,targetPosition=R[j1], maxVelocity = maxvel[0])
-        p.setJointMotorControl2(s1,1, control,targetPosition=L[j1], maxVelocity = maxvel[1])
-        j1+=1
-    if i%dt==n2:
-        j2 = j2%len(R)
-        p.setJointMotorControl2(s2,0, control,targetPosition=R[j2], maxVelocity = maxvel[2])
-        p.setJointMotorControl2(s2,1, control,targetPosition=L[j2], maxVelocity = maxvel[3])
-        j2+=1
-    if i%dt==n3:
-        j3 = j3%len(R)
-        p.setJointMotorControl2(s3,0, control,targetPosition=R[j3], maxVelocity = maxvel[4])
-        p.setJointMotorControl2(s3,1, control,targetPosition=L[j3], maxVelocity = maxvel[5])
-        j3+=1
-    if i%dt==n4:
-        j4 = j4%len(R)
-        p.setJointMotorControl2(s4,0, control,targetPosition=R[j4], maxVelocity = maxvel[6])
-        p.setJointMotorControl2(s4,1, control,targetPosition=L[j4], maxVelocity = maxvel[7])
-        j4+=1
-    if i%dt==n5:
-        j5 = j5%len(R)
-        p.setJointMotorControl2(s5,0, control,targetPosition=0, maxVelocity = maxvel[8])
-        p.setJointMotorControl2(s5,1, control,targetPosition=0, maxVelocity = maxvel[9])
-        j5+=1
+    time.sleep(1./240.)
+    if i%dt==s1.gait_phase:
+        s1.motor_step()
+    if i%dt==s2.gait_phase:
+        s2.motor_step()
+    if i%dt==s3.gait_phase:
+        s3.motor_step()
+    if i%dt==s4.gait_phase:
+        s4.motor_step()
+    if i%dt==s5.gait_phase:
+        s5.motor_step()
+
 
 cubePos, cubeOrn = p.getBasePositionAndOrientation(s1)
 print(cubePos,cubeOrn)
--- src/smarticle_simulation_object.py
+++ src/smarticle_simulation_object.py
@@ -1,6 +1,33 @@
 # smarticle_simulation_object
 
+import numpy as np
+from pdb import set_trace as bp
+
 class SmarticleSimulationObject(object):
     """docstring for SmarticleSimulationObject."""
+    MAX_VEL_RANGE = 0.2
+    MAX_ANGLE_OFFSET = 0.5
 
-    def __init__(self, p, id, x0, urdf_path, max_vel, gait_dt= 0.45):
+    def __init__(self, p, urdf_path, max_vel, basePosition):
+
+        self.p = p
+        self.id = self.p.loadURDF(urdf_path, basePosition = basePosition)
+        self.maxvel = (max_vel-self.MAX_VEL_RANGE/2.)*np.ones(2)+\
+            self.MAX_VEL_RANGE*np.random.rand(2)
+        self.control = self.p.POSITION_CONTROL
+
+    def load_gait(self, gait, gait_dt):
+        '''DOC'''
+        self.n = gait.shape[1]
+        self.gait_index = np.random.randint(0,self.n)
+        self.gait_period = gait_dt
+        self.gait_phase = int(self.gait_period*np.random.rand())
+        self.gaitL = self.MAX_ANGLE_OFFSET*np.random.rand(self.n)-self.MAX_ANGLE_OFFSET/2.+gait[0]
+        self.gaitR = self.MAX_ANGLE_OFFSET*np.random.rand(self.n)-self.MAX_ANGLE_OFFSET/2.+gait[1]
+
+    def motor_step(self):
+        self.p.setJointMotorControl2(self.id,0, self.control,\
+            targetPosition=self.gaitR[self.gait_index], maxVelocity = int(self.maxvel[0]))
+        self.p.setJointMotorControl2(self.id,1, self.control,\
+            targetPosition=self.gaitL[self.gait_index], maxVelocity = self.maxvel[1])
+        self.gait_index= (self.gait_index+1)%self.n
