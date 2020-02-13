9d490e1e97c7234eb6343579958ecb09abec647c
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-13 12:45:32 -0600

    begin work on smarticle simulation objects

---
+++ src/smarticle_simulation_object.py
@@ -0,0 +1,6 @@
+# smarticle_simulation_object
+
+class SmarticleSimulationObject(object):
+    """docstring for SmarticleSimulationObject."""
+
+    def __init__(self, p, id, x0, urdf_path, max_vel, gait_dt= 0.45):
