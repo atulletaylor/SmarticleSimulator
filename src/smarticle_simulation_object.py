dffc999b26614124885c5cd09f2c323a3c107fa7
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-14 16:13:52 -0600

    begin work on static light experiment

--- src/smarticle_simulation_object.py
+++ src/smarticle_simulation_object.py
@@ -12,9 +12,12 @@ class SmarticleSimulationObject(object):
 
         self.p = p
         self.id = self.p.loadURDF(urdf_path, basePosition = basePosition)
-        self.maxvel = (max_vel-self.MAX_VEL_RANGE/2.)*np.ones(2)+\
-            self.MAX_VEL_RANGE*np.random.rand(2)
+        self.maxvel = (max_vel-self.MAX_VEL_RANGE/2.)*np.ones(2)\
+            + self.MAX_VEL_RANGE*np.random.rand(2)
         self.control = self.p.POSITION_CONTROL
+        self.plank = 0
+        self.arm_offset = self.MAX_ANGLE_OFFSET*np.random.rand(2)\
+            - self.MAX_ANGLE_OFFSET/2.
 
     def load_gait(self, gait, gait_dt):
         '''DOC'''
@@ -22,12 +25,30 @@ class SmarticleSimulationObject(object):
         self.gait_index = np.random.randint(0,self.n)
         self.gait_period = gait_dt
         self.gait_phase = int(self.gait_period*np.random.rand())
-        self.gaitL = self.MAX_ANGLE_OFFSET*np.random.rand(self.n)-self.MAX_ANGLE_OFFSET/2.+gait[0]
-        self.gaitR = self.MAX_ANGLE_OFFSET*np.random.rand(self.n)-self.MAX_ANGLE_OFFSET/2.+gait[1]
+        self.gaitL = self.arm_offset[0]+gait[0]
+        self.gaitR = self.arm_offset[1]+gait[1]
+
+    def move_arms(self,posL, posR):
+        self.p.setJointMotorControl2(self.id,0, self.control,\
+            targetPosition=posR, maxVelocity = int(self.maxvel[0]))
+        self.p.setJointMotorControl2(self.id,1, self.control,\
+            targetPosition=posL, maxVelocity = int(self.maxvel[0]))
+
+    def set_plank(self, state):
+        if state==1:
+            self.plank = 1
+            self.move_arms(self.arm_offset[0],self.arm_offset[1])
 
     def motor_step(self):
-        self.p.setJointMotorControl2(self.id,0, self.control,\
-            targetPosition=self.gaitR[self.gait_index], maxVelocity = int(self.maxvel[0]))
-        self.p.setJointMotorControl2(self.id,1, self.control,\
-            targetPosition=self.gaitL[self.gait_index], maxVelocity = self.maxvel[1])
-        self.gait_index= (self.gait_index+1)%self.n
+        if self.plank !=1:
+            posL = self.gaitL[self.gait_index]
+            posR = self.gaitR[self.gait_index]
+        else:
+            posL = self.arm_offset[0]
+            posR = self.arm_offset[1]
+        self.move_arms(posL, posR)
+        self.gait_index = (self.gait_index+1)%self.n
+
+    def get_position(self):
+        pos, orient = self.p.getBasePositionAndOrientation(self.id)
+        self.x = np.array([pos[0],pos[1],self.p.getEulerFromQuaternion(orient)[2]])
