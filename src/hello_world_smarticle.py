6a80e268feff4537806b3539e636256160c3b4df
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-16 17:23:19 -0600

    urdf

--- src/hello_world_smarticle.py
+++ src/hello_world_smarticle.py
@@ -25,29 +25,29 @@ dt = time_to_steps(0.45)
 x = 0.02
 r = p.loadURDF(ring_path, basePosition = [0,0,0])
 s1 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x+0.03,0])
-s2 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x+0,0])
-s3 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x-0.03,0])
-s4 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x-0.06,0])
-s5 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x-0.09,0])
-smarticles = [s1, s2, s3, s4, s5]
-R = [-1.7,1.7,1.7,-1.7]
-L = [1.7,1.7,-1.7,-1.7]
-for s in smarticles:
-    s.load_gait(np.array([L,R]),dt)
+# s2 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x+0,0])
+# s3 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x-0.03,0])
+# s4 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x-0.06,0])
+# s5 = ss(p, urdf_path, maxvel, basePosition = [-0.027,x-0.09,0])
+# smarticles = [s1, s2, s3, s4, s5]
+# R = [-1.7,1.7,1.7,-1.7]
+# L = [1.7,1.7,-1.7,-1.7]
+# for s in smarticles:
+#     s.load_gait(np.array([L,R]),dt)
 
 
 t_steps = time_to_steps(1200)
 for i in range (t_steps):
     p.stepSimulation()
     time.sleep(1./240.)
-    if i%dt==s1.gait_phase:
-        s1.motor_step()
-    if i%dt==s2.gait_phase:
-        s2.motor_step()
-    if i%dt==s3.gait_phase:
-        s3.motor_step()
-    if i%dt==s4.gait_phase:
-        s4.motor_step()
-    if i%dt==s5.gait_phase:
-        s5.motor_step()
+    # if i%dt==s1.gait_phase:
+    #     s1.motor_step()
+    # if i%dt==s2.gait_phase:
+    #     s2.motor_step()
+    # if i%dt==s3.gait_phase:
+    #     s3.motor_step()
+    # if i%dt==s4.gait_phase:
+    #     s4.motor_step()
+    # if i%dt==s5.gait_phase:
+    #     s5.motor_step()
 p.disconnect()
