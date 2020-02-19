a61bfcc58a266deee7cb6c6234bcee336c80ccdb
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-18 19:43:02 -0600

    create load smarticles helper

--- src/hello_world_smarticle.py
+++ src/hello_world_smarticle.py
@@ -14,35 +14,34 @@ ring_path = '../urdf/ring.urdf'
 def time_to_steps(time_s):
     return int(time_s*240)
 
+def load_smarticles(n,urdf_path, max_vel, dx,th,gait,dt):
+    smarticles = []
+    offset = -dx*(n//2)
+    for ii in range(n):
+        x = offset+ii*dx
+        smarticles.append(ss(p, urdf_path, maxvel, basePosition = [0,x,0],\
+                             baseOrientation = [0,0,th]))
+        smarticles[-1].load_gait(np.array(gait),dt)
+
+    return smarticles
+
+
 
 
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-maxvel = 6.
 dt = time_to_steps(0.45)
-x = 0.02
 r = p.loadURDF(ring_path, basePosition = [0,0,0])
 cid = p.createConstraint(r,-1,0,-1,p.JOINT_PRISMATIC,[0,0,0],[0,0,0],[0,0,0])
+maxvel = 6.
 dx = 0.032
 th = np.pi/2
-s1 = ss(p, urdf_path, maxvel, basePosition = [0,-2*dx,0], baseOrientation = [0,0,th])
-s2 = ss(p, urdf_path, maxvel, basePosition = [0,-dx,0], baseOrientation = [0,0,th])
-s3 = ss(p, urdf_path, maxvel, basePosition = [0,0,0], baseOrientation = [0,0,th])
-s4 = ss(p, urdf_path, maxvel, basePosition = [0,dx,0], baseOrientation = [0,0,th])
-s5 = ss(p, urdf_path, maxvel, basePosition = [0,2*dx,0], baseOrientation = [0,0,th])
-bp()
-smarticles = [s1]
-
-smarticles = [s1, s2, s3, s4]
-
 R = [-1.7,1.7,1.7,-1.7]
 L = [1.7,1.7,-1.7,-1.7]
-for s in smarticles:
-    s.load_gait(np.array([L,R]),dt)
+
+s = load_smarticles(5,urdf_path, maxvel, dx,th,[L,R],dt)
 
 for i in range (480):
     p.stepSimulation()
@@ -51,16 +50,16 @@ t_steps = time_to_steps(1200)
 for i in range (t_steps):
     p.stepSimulation()
     time.sleep(1./240.)
-    if i%dt==s1.gait_phase:
-        s1.motor_step()
-        s1.update_position()
+    if i%dt==s[0].gait_phase:
+        s[0].motor_step()
+        s[0].update_position()
         # bp()
-    if i%dt==s2.gait_phase:
-        s2.motor_step()
-    if i%dt==s3.gait_phase:
-        s3.motor_step()
-    if i%dt==s4.gait_phase:
-        s4.motor_step()
-    # if i%dt==s5.gait_phase:
-    #     s5.motor_step()
+    if i%dt==s[1].gait_phase:
+        s[1].motor_step()
+    if i%dt==s[2].gait_phase:
+        s[2].motor_step()
+    if i%dt==s[3].gait_phase:
+        s[3].motor_step()
+    # if i%dt==s[4].gait_phase:
+    #     s[4].motor_step()
 p.disconnect()
