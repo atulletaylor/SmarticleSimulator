cf26f70954db9ad8847447a2c13069c70dbf601a
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-26 10:43:43 -0600

    update hello world

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,31 +1,12 @@
 import pybullet as p
 import time
 import pybullet_data
-from simulation_smarticle import SimulationSmarticle as ss
-from flashlight import Flashlight
-
-from pdb import set_trace as bp
-
+import smarticlesimulation as sim
 import numpy as np
 
 #URDF paths
 smarticle_path = '../urdf/smarticle.urdf'
 ring_path = '../urdf/ring.urdf'
-
-def time_to_steps(time_s):
-    return int(time_s*240)
-
-def load_smarticles(n,urdf_path, max_vel, dx,th,gait,dt,z):
-    smarticles = []
-    offset = -dx*(n//2)
-    for ii in range(n):
-        x = offset+ii*dx
-        smarticles.append(ss(urdf_path, maxvel, basePosition = [0,x,z],\
-                             baseOrientation = [0,0,th]))
-        smarticles[-1].load_gait(np.array(gait),dt)
-
-    return smarticles
-
 
 
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
@@ -42,10 +23,10 @@ dx = 0.032
 th = np.pi/2
 R = [-1.7,1.7,1.7,-1.7]
 L = [1.7,1.7,-1.7,-1.7]
-dt = time_to_steps(0.45)
+dt = sim.time_to_steps(0.45)
 z = 0.6
 
-smarticles = load_smarticles(n,smarticle_path, maxvel, dx,th,[L,R],dt,z)
+smarticles = sim.load_smarticles(n,smarticle_path, maxvel, dx,th,[L,R],dt,z)
 r = p.loadURDF(ring_path, basePosition = [0,0,z])
 
 for i in range (2*480):
