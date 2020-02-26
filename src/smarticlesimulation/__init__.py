c4a4d27471024aa23ef427be83fd5a0eb903d7f5
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-26 11:24:19 -0600

    update init

--- src/smarticlesimulation/__init__.py
+++ src/smarticlesimulation/__init__.py
@@ -1,5 +1,2 @@
-from helpers import time_to_steps
-from helpers import ray_check
-from helpers import load_smarticles
-from flashlight import Flashlight
-from smarticle import Smarticle
+from .smarticle import Smarticle
+from .helpers import time_to_steps, load_smarticles
