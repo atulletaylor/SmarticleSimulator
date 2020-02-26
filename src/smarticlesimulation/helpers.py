36991953600e24cc40d8ed782e69040a2fbf7133
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-26 11:26:41 -0600

    fixed import and bugs

--- src/smarticlesimulation/helpers.py
+++ src/smarticlesimulation/helpers.py
@@ -1,11 +1,11 @@
 import numpy as np
-from smarticle import Smarticle
+from .smarticle import Smarticle
 
 def time_to_steps(time_s):
     return int(time_s*240)
 
 
-def load_smarticles(n,urdf_path, max_vel, dx,th,gait,dt,z):
+def load_smarticles(n,urdf_path, maxvel, dx,th,gait,dt,z):
     smarticles = []
     offset = -dx*(n//2)
     for ii in range(n):
