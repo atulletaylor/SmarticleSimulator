1c27837a1cf19e807faa60e0858e2dc3db2f2fcc
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-26 10:48:05 -0600

    update helpers

--- src/smarticlesimulation/helpers.py
+++ src/smarticlesimulation/helpers.py
@@ -1,23 +1,27 @@
+import numpy as np
+import pybullet as p
+from smarticle import Smarticle
 
 def time_to_steps(time_s):
     return int(time_s*240)
+
 
 def load_smarticles(n,urdf_path, max_vel, dx,th,gait,dt,z):
     smarticles = []
     offset = -dx*(n//2)
     for ii in range(n):
         x = offset+ii*dx
-        smarticles.append(ss(urdf_path, maxvel, basePosition = [0,x,z],\
-                             baseOrientation = [0,0,th]))
+        smarticles.append(Smarticle(urdf_path, maxvel, basePosition = [0,x,z],\
+                                    baseOrientation = [0,0,th]))
         smarticles[-1].load_gait(np.array(gait),dt)
-
     return smarticles
 
-def ray_check(s,fl):
+
+def ray_check(smarticles,fl):
     p.removeAllUserDebugItems()
     results = fl.draw_rays()
-    smart_ids = [x.id for x in s]
-    for smart in s:
+    smart_ids = [x.id for x in smarticles]
+    for smart in smarticles:
         smart.update_position()
         smart.set_plank(0)
     for ray in results:
